f47b248306ac73d8be34d9323dc2b271c2294064
Laura Fast <90442790+LauraSofieFast@users.noreply.github.com>
2022-10-04 09:39:05 +0200

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -6,7 +6,7 @@ const cacheName = 'chache-insects';
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(cacheName).then(function (cache) {
-            return cache.addAll(['/insects/', '/insects/index.html', '/insects/butterflies.jpg', '/insects/butterfly.jpg', '/index/dragonfly.jpg']);
+            return cache.addAll(['/insects/', '/insects/index.html', '/insects/butterflies.jpg', '/insects/butterfly.jpg', '/insects//dragonfly.jpg']);
         })
     );
 });
